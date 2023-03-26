f7f68bae8b6f9af988451e77c01fc9e6a07d8f5a
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-03-26 07:48:06 +0000

    [Add]5-14投稿機能作成＆ユーザー情報編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+
+  resources :post_images, only: [:new, :create, :index, :show ,:destroy]
+  resources :users, only: [:show, :edit]
+  get 'homes/about', to: 'homes#about' , as:'about'
 end
