cca4141eee87696dd45142cbd851d617db1701d8
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-03-29 14:28:03 +0000

    [Add]5-22kaminariインストール

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 
-  resources :post_images, only: [:new, :create, :index, :show ,:destroy]
-  resources :users, only: [:show, :edit]
+  resources :post_images, only: [:new, :create, :index, :show ,:destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
   get 'homes/about', to: 'homes#about' , as:'about'
+
 end
